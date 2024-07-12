5dd6b92613903c79ca6372e5abfff9901e7ead81
Nethala Nikhil <109343084+NethalaNikhil@users.noreply.github.com>
2024-07-12 13:34:36 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-document.getElementById('call').addEventListener('click',function(){
-    
+document.querySelectorAll('[data-bs-toggle="popover"]')
+.forEach(popover => {
+  new bootstrap.Popover(popover)
 })
+
+
+
+document.getElementById('call').addEventListener('click', function() {
+    var img = document.getElementById('buttonImage');
+    if (img.src.includes('call_o_o.png')) {
+      img.src = 'https://kona-digital.com/images/call_c_c.png';
+    } else {
+      img.src = 'https://kona-digital.com/images/call_o_o.png';
+    }
+  });
